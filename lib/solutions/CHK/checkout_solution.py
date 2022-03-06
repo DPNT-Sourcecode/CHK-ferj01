13ae3623a8970564fb1bb80288a2253d25032906
sbx_user1051 <sbx_user1051@169.254.6.253>
2022-03-06 10:30:18 +0000

    Sun Mar 06 10:30:18 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -48,7 +48,13 @@ class ProductD(Product):
     offer_num = 0
     offer_price = 0
 
-        
+PRODUCT_MAP = {
+    "A": ProductA(),
+    "B": ProductB(),
+    "C": ProductC(),
+    "D": ProductD(),
+}
+
 
 def checkout(skus):
     items = Counter([i for i in skus])
@@ -59,9 +65,8 @@ def checkout(skus):
 
     total = 0
     for k, count in items.items():
-        product = Product()
-        price = product.get_price(count)
-        total += price
+        product = PRODUCT_MAP[k]
+        total += product.get_price(count)
 
     return total
 
@@ -78,4 +83,20 @@ assert checkout("AA") == 100
 assert checkout("AAA") == 130
 assert checkout("AAAA") == 180
 assert checkout("AAAAAA") == 260
+
+assert checkout("B") == 30
+assert checkout("BB") == 45
+assert checkout("BBB") == 75
+assert checkout("BBBB") == 90
+assert checkout("BBBBB") == 120
+
+assert checkout("C") == 20
+assert checkout("CC") == 40
+assert checkout("CCC") == 60
+assert checkout("CCCC") == 80
+
+assert checkout("D") == 15
+assert checkout("DD") == 30
+assert checkout("DDD") == 45
+assert checkout("DDDD") == 60
 
